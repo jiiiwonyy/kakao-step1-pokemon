13fef7e897d9d31dae5a7f5c74082d5bace7e4cc
jiiiwonyy <ji__won_22@naver.com>
2025-05-22 05:04:39 +0900

    design: 홈화면 ui 수정

--- pokemondex/src/Pages/Home.jsx
+++ pokemondex/src/Pages/Home.jsx
@@ -5,16 +5,25 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <>
+    <HomeContainer>
       <PokemonLogo src="public/pokemon-logo.png" alt="pokemon logo" />
       <StartButton onClick={() => navigate("/dex")}>
         포켓몬 도감 시작하기
       </StartButton>
-    </>
+    </HomeContainer>
   );
 };
 
 export default Home;
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+  background-color: white;
+`;
 
 const PokemonLogo = styled.img`
   width: 100%;
@@ -29,4 +38,8 @@ const PokemonLogo = styled.img`
 const StartButton = styled.button`
   background-color: #ff0000;
   color: white;
+  cursor: pointer;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 5px;
 `;
