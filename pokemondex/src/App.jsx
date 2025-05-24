3424e835f25fd84df43344ac45b3f2fe12a6e86d
jiiiwonyy <ji__won_22@naver.com>
2025-05-25 04:02:59 +0900

    feat: toast ui

--- pokemondex/src/App.jsx
+++ pokemondex/src/App.jsx
@@ -1,8 +1,15 @@
 import Router from "./shared/Router";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <ToastContainer position="top-center" autoClose={2000} />
+    </>
+  );
 }
 
 export default App;
