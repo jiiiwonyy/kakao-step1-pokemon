c41dfa761b49987b9a8809883715fb58501a501e
jiiiwonyy <ji__won_22@naver.com>
2025-05-25 03:16:10 +0900

    feat: redux-persist 사용해서 localstorage 구현

--- pokemondex/src/redux/config/configStore.js
+++ pokemondex/src/redux/config/configStore.js
@@ -1,8 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import pokemonReducer from "../modules/pokemonSlices";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  pokemon: pokemonReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    pokemon: pokemonReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
